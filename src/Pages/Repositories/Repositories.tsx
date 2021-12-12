4482eb3b9266ee63f8805e27f312d21aae76c98b
arqamali4 <arqamali@carmatech.in>
2021-12-12 23:06:11 +0400

    added loader

--- src/Pages/Repositories/Repositories.tsx
+++ src/Pages/Repositories/Repositories.tsx
@@ -10,6 +10,7 @@ import {
   Radio,
   Dropdown,
   Menu,
+  Spin,
 } from "antd";
 import { useValues } from "kea";
 import {
@@ -23,7 +24,7 @@ import ReposetoriesLogic from "../../Redux/repositoriesLogic";
 import _ from "lodash";
 
 const Repositories = () => {
-  const { reposetories } = useValues(ReposetoriesLogic);
+  const { loading, reposetories } = useValues(ReposetoriesLogic);
 
   const IconText = ({ icon, text }: any) => (
     <Space>
@@ -47,77 +48,79 @@ const Repositories = () => {
   );
 
   return (
-    <Row gutter={[24, 24]}>
-      <Col span={24} className="flex-display header">
-        <Typography.Title level={4}>Trending</Typography.Title>
-        <p>See what the GitHub community is most excited about today.</p>
-      </Col>
-      <Col span={24}>
-        <Card
-          className="repo-list"
-          title={
-            <Radio.Group
-              defaultValue="repositories"
-              optionType="button"
-              buttonStyle="solid"
-            >
-              <Radio.Button value="repositories">Repositories</Radio.Button>
-              <Radio.Button value="developer">Developers</Radio.Button>
-            </Radio.Group>
-          }
-        >
-          <List
-            itemLayout="vertical"
-            size="large"
-            dataSource={reposetories}
-            renderItem={(item: any) => (
-              <List.Item
-                key={item.title}
-                actions={[
-                  <span>{item.language}</span>,
-                  <IconText
-                    icon={StarOutlined}
-                    text={item.totalStars}
-                    key="list-vertical-star-o"
-                  />,
-                  <IconText
-                    icon={ForkOutlined}
-                    text={item.forks}
-                    key="list-vertical-message"
-                  />,
-                  <Space>
-                    <span>built by</span>
-                    {_.map(item.builtBy, (each) => (
-                      <Avatar size="small" src={each.avatar} />
-                    ))}
-                  </Space>,
-                ]}
-                extra={
-                  <Dropdown.Button overlay={menu}>
-                    <StarOutlined />
-                    Star
-                  </Dropdown.Button>
-                }
+    <Spin spinning={loading}>
+      <Row gutter={[24, 24]}>
+        <Col span={24} className="flex-display header">
+          <Typography.Title level={4}>Trending</Typography.Title>
+          <p>See what the GitHub community is most excited about today.</p>
+        </Col>
+        <Col span={24}>
+          <Card
+            className="repo-list"
+            title={
+              <Radio.Group
+                defaultValue="repositories"
+                optionType="button"
+                buttonStyle="solid"
               >
-                <List.Item.Meta
-                  //   avatar={<BookOutlined className="font-color-8b949e" />}
-                  title={
+                <Radio.Button value="repositories">Repositories</Radio.Button>
+                <Radio.Button value="developer">Developers</Radio.Button>
+              </Radio.Group>
+            }
+          >
+            <List
+              itemLayout="vertical"
+              size="large"
+              dataSource={reposetories}
+              renderItem={(item: any) => (
+                <List.Item
+                  key={item.title}
+                  actions={[
+                    <span>{item.language}</span>,
+                    <IconText
+                      icon={StarOutlined}
+                      text={item.totalStars}
+                      key="list-vertical-star-o"
+                    />,
+                    <IconText
+                      icon={ForkOutlined}
+                      text={item.forks}
+                      key="list-vertical-message"
+                    />,
                     <Space>
-                      <BookOutlined className="font-color-8b949e" />
-                      <a
-                        href={item.href}
-                      >{`${item.username} / ${item.repositoryName}`}</a>
-                    </Space>
+                      <span>built by</span>
+                      {_.map(item.builtBy, (each) => (
+                        <Avatar size="small" src={each.avatar} />
+                      ))}
+                    </Space>,
+                  ]}
+                  extra={
+                    <Dropdown.Button overlay={menu}>
+                      <StarOutlined />
+                      Star
+                    </Dropdown.Button>
                   }
-                  description={item.description}
-                />
-                {item.content}
-              </List.Item>
-            )}
-          />
-        </Card>
-      </Col>
-    </Row>
+                >
+                  <List.Item.Meta
+                    //   avatar={<BookOutlined className="font-color-8b949e" />}
+                    title={
+                      <Space>
+                        <BookOutlined className="font-color-8b949e" />
+                        <a
+                          href={item.href}
+                        >{`${item.username} / ${item.repositoryName}`}</a>
+                      </Space>
+                    }
+                    description={item.description}
+                  />
+                  {item.content}
+                </List.Item>
+              )}
+            />
+          </Card>
+        </Col>
+      </Row>
+    </Spin>
   );
 };
 
